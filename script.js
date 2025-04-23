f951d0f01087c9c63fe358fa42092889b139667a
Tushar-Mehra1707 <tusharmehra4321@gmail.com>
2025-04-23 20:00:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
 //complete this code
+// Rectangle class
+class Rectangle {
+  constructor(width, height) {
+    if (width <= 0 || height <= 0) {
+      throw new Error("Width and height must be positive integers.");
+    }
+    this._width = width;
+    this._height = height;
+  }
+
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+// Square class inheriting from Rectangle
+class Square extends Rectangle {
+  constructor(side) {
+    if (side <= 0) {
+      throw new Error("Side must be a positive integer.");
+    }
+    super(side, side); // Set both width and height to side
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return 4 * this.width; // or 4 * this._width
+  }
+
+
 class Rectangle {}
 
 class Square extends Animal {}
